e987cbca152640d135fd410365e64742ab1fcab2
saman-dot-cloud <e.saman1701@gmail.com>
2026-08-18 16:28:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// Konfigurasi otomatis untuk mengaktifkan React dan Tailwind CSS v4
 export default defineConfig({
+  // KUNCI UTAMA: Memaksa semua aset tombol dan CSS menggunakan jalur relatif yang dikenal Netlify
+  base: './', 
   plugins: [
     react({
       jsxRuntime: 'automatic'
     }),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
