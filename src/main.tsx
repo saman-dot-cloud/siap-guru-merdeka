72f1c89868ae5f8d3328dc6d0aaa8a9322f63276
saman-dot-cloud <e.saman1701@gmail.com>
2026-08-18 15:55:02 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Link, Navigate, Route, Routes, useNavigate } from 'react-router'
